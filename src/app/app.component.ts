0584ab8a34d477470dffed4bdf6036dafcc8072b
Jeffery Edge <jefferyedge1@gmail.com>
2024-05-10 10:14:25 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UnderConstructionComponent } from "./under-construction/under-construction.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, UnderConstructionComponent]
 })
 export class AppComponent {
   title = 'paper-and-pixels-website';
